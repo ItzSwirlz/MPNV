944fac1e939b3f18b9dca1a762b4e1c240818e4a
Joshua Peisach <itzswirlz2020@outlook.com>
2024-01-21 16:38:39 -0500

    Check mersenne primes

--- src/main/java/com/github/ItzSwirlz/mpnv/MPNV.java
+++ src/main/java/com/github/ItzSwirlz/mpnv/MPNV.java
@@ -12,8 +12,33 @@ import net.minecraft.util.math.BlockPos;
 import static net.minecraft.server.command.CommandManager.*;
 
 public class MPNV implements ModInitializer {
-	// stolen from https://dev.to/priyanka__488/number-theory-primality-test-in-o-sqrt-n-dde
-	// Primality test in O(sqrt(n)) time
+	public class CalculationResults {
+		public boolean isPrime = true;
+		public boolean isMersennePrime = false;
+
+		public CalculationResults(int num) {
+			/*
+			 * Regular primality test
+			 * src: https://dev.to/priyanka__488/number-theory-primality-test-in-o-sqrt-n-dde
+			 * 
+			 * By default, assume the number is prime.
+			 */ 
+			if(num == 1) isPrime = false;
+			
+			for (int i = 2; i * i < num; i++) {
+				if (num % i == 0) isPrime = false;
+			}
+			// If the number is prime, isPrime will still be true.
+
+			if(isPrime) {
+				/*
+				 * Mersenne Primality Test
+				 */
+				if((Math.log(num + 1) / Math.log(2)) % 1 == 0) isMersennePrime = true;
+			}
+		}
+	}
+
 	public static boolean isPrime(int num) {
 		if(num == 1) return false;
 
@@ -26,6 +51,7 @@ public class MPNV implements ModInitializer {
 
 	@Override
 	public void onInitialize(ModContainer mod) {
+		// FIXME: this needs massive cleanup
 		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> dispatcher.register(literal("mpnv2d")
 			.then(argument("operation", StringArgumentType.string())
 			.then(argument("radius", IntegerArgumentType.integer())
@@ -34,45 +60,77 @@ public class MPNV implements ModInitializer {
 				final int radius = IntegerArgumentType.getInteger(context, "radius");
 
 				for(int x = 0; x < radius; x++) {
+					CalculationResults calculationResultsX = new CalculationResults(x);
 					if(operation.equals("and")) {
 						for(int z = 0; z < radius; z++) {
-							if(isPrime(x) && isPrime(z)) {
-								context.getSource().getWorld().setBlockState(new BlockPos(x, 0, z), Blocks.STONE.getDefaultState());
+							CalculationResults calculationResultsZ = new CalculationResults(z);
+							if(calculationResultsX.isPrime && calculationResultsZ.isPrime) {
+								System.out.println("(" + x + ", " + z + ") is x prime: " + calculationResultsX.isPrime + "is z prime: " + calculationResultsZ.isPrime + " is x mersenne: " + calculationResultsX.isMersennePrime + " is z mersenne: " + calculationResultsZ.isMersennePrime);
+								if(calculationResultsX.isMersennePrime && calculationResultsZ.isMersennePrime) {
+									context.getSource().getWorld().setBlockState(new BlockPos(x, 0, z), Blocks.GOLD_BLOCK.getDefaultState());
+								} else {
+									context.getSource().getWorld().setBlockState(new BlockPos(x, 0, z), Blocks.STONE.getDefaultState());
+								}
 							}
 						}
 					}
 					else if(operation.equals("or")) {
 						for(int z = 0; z < radius; z++) {
-							if(isPrime(x) || isPrime(z)) {
-								context.getSource().getWorld().setBlockState(new BlockPos(x, 0, z), Blocks.STONE.getDefaultState());
+							CalculationResults calculationResultsZ = new CalculationResults(z);
+							if(calculationResultsX.isPrime || calculationResultsZ.isPrime) {
+								if(calculationResultsX.isMersennePrime || calculationResultsZ.isMersennePrime) {
+									context.getSource().getWorld().setBlockState(new BlockPos(x, 0, z), Blocks.GOLD_BLOCK.getDefaultState());
+								} else {
+									context.getSource().getWorld().setBlockState(new BlockPos(x, 0, z), Blocks.STONE.getDefaultState());
+								}
 							}
 						}
 					}
 					else if(operation.equals("neither") || operation.equals("nor")) {
 						for(int z = 0; z < radius; z++) {
-							if(!isPrime(x) && !isPrime(z)) {
-								context.getSource().getWorld().setBlockState(new BlockPos(x, 0, z), Blocks.STONE.getDefaultState());
+							CalculationResults calculationResultsZ = new CalculationResults(z);
+							if(!calculationResultsX.isPrime && !calculationResultsZ.isPrime) {
+								if(!calculationResultsX.isMersennePrime && !calculationResultsZ.isMersennePrime) {
+									context.getSource().getWorld().setBlockState(new BlockPos(x, 0, z), Blocks.GOLD_BLOCK.getDefaultState());
+								} else {
+									context.getSource().getWorld().setBlockState(new BlockPos(x, 0, z), Blocks.STONE.getDefaultState());
+								}
 							}
 						}
 					}
 					else if(operation.equals("plus")) {
 						for(int z = 0; z < radius; z++) {
-							if(isPrime(x + z)) {
-								context.getSource().getWorld().setBlockState(new BlockPos(x, 0, z), Blocks.STONE.getDefaultState());
+							CalculationResults calculationResults = new CalculationResults(x + z);
+							if(calculationResults.isPrime) {
+								if(calculationResults.isMersennePrime) {
+									context.getSource().getWorld().setBlockState(new BlockPos(x, 0, z), Blocks.GOLD_BLOCK.getDefaultState());
+								} else {
+									context.getSource().getWorld().setBlockState(new BlockPos(x, 0, z), Blocks.STONE.getDefaultState());
+								}
 							}
 						}
 					}
 					else if(operation.equals("subtract")) {
 						for(int z = 0; z < radius; z++) {
-							if(isPrime(x - z)) {
-								context.getSource().getWorld().setBlockState(new BlockPos(x, 0, z), Blocks.STONE.getDefaultState());
+							CalculationResults calculationResults = new CalculationResults(x - z);
+							if(calculationResults.isPrime) {
+								if(calculationResults.isMersennePrime) {
+									context.getSource().getWorld().setBlockState(new BlockPos(x, 0, z), Blocks.GOLD_BLOCK.getDefaultState());
+								} else {
+									context.getSource().getWorld().setBlockState(new BlockPos(x, 0, z), Blocks.STONE.getDefaultState());
+								}
 							}
 						}
 					}
 					else if(operation.equals("divide")) {
 						for(int z = 1; z < radius; z++) {
-							if(isPrime(x / z)) {
-								context.getSource().getWorld().setBlockState(new BlockPos(x, 0, z), Blocks.STONE.getDefaultState());
+							CalculationResults calculationResults = new CalculationResults(x / z);
+							if(calculationResults.isPrime) {
+								if(calculationResults.isMersennePrime) {
+									context.getSource().getWorld().setBlockState(new BlockPos(x, 0, z), Blocks.GOLD_BLOCK.getDefaultState());
+								} else {
+									context.getSource().getWorld().setBlockState(new BlockPos(x, 0, z), Blocks.STONE.getDefaultState());
+								}
 							}
 						}
 					}
